c6b5df85661f411b778a269a5a1ca20b387c6488
Martin Chaperot <martin.chapino@gmail.com>
2023-09-08 20:56:57 -0400

    Setup CSS + icon libraries

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,27 @@
 import { createApp } from "vue";
-import "./styles.css";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+// Vuetify
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import { aliases, md } from "vuetify/iconsets/md";
+import 'vuetify/styles';
+
+// Custom styles
+import "./styles.css";
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    icons: {
+        defaultSet: 'md',
+        aliases,
+        sets: {
+            md,
+        },
+    }
+})
+
+createApp(App).use(vuetify).mount("#app");
