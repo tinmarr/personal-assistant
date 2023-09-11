501aa194da9a3772b04a251e5c017ae56dfae0a8
Martin Chaperot <martin.chapino@gmail.com>
2023-09-10 22:35:29 -0400

    Supabase auth

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
+<script setup lang="ts">
+import { createClient } from '@supabase/supabase-js';
+
+const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_KEY);
+
+const signIn = async () => {
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider: 'google',
+  });
+  console.log(data, error)
+}
+</script>
+
 <template>
+<v-btn @click="signIn">sign in</v-btn>
 
 </template>
-
-<script setup lang="ts">
-</script>
